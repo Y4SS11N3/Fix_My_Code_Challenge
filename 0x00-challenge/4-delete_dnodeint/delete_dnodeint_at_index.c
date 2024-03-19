5d4884834af53e8c9b64fe9272760201623a1ff0
YASSINE <contact@yassine.fun>
2024-03-19 16:19:12 -0700

    Fix delete node function in double linked list

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -33,13 +33,17 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (current == NULL)
 		return (-1);
 
-	if (current->next != NULL)
-		current->next->prev = current->prev;
-	if (current->prev != NULL)
-		current->prev->next = current->next;
+	dlistint_t *next_node = current->next;
+	dlistint_t *prev_node = current->prev;
+
+	if (next_node != NULL)
+		next_node->prev = prev_node;
+	if (prev_node != NULL)
+		prev_node->next = next_node;
 	else
-		*head = current->next;
+		*head = next_node;
 
 	free(current);
 	return (1);
 }
+
