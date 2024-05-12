4f77f49fe4800b2d792676d95eb6b19208f08bd9
YASSINE <contact@yassine.fun>
2024-05-12 06:31:33 -0700

    Step 2: User model

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -4,27 +4,26 @@ User class
 """
 
 class User():
-    """ Documentation """
+    """ Class to represent a User """
 
     def __init__(self):
-        """ Documentation """
+        """ Initialize with email as None """
         self.__email = None
-
-    @email.setter
-    def email(self, value):
-        """ Documentation """
-        if type(value) is not str:
-            raise TypeError("email must be a string")
-        self.__email = value
 
     @property
     def email(self):
-        """ Documentation """
+        """ Getter for email """
         return self.__email
-   
-    
-if __name__ == "__main__":
 
+    @email.setter
+    def email(self, value):
+        """ Setter for email, checks if value is a string """
+        if not isinstance(value, str):
+            raise TypeError("Email must be a string")
+        self.__email = value
+
+if __name__ == "__main__":
+    "Execute main block."
     u = User()
     u.email = "john@snow.com"
     print(u.email)
