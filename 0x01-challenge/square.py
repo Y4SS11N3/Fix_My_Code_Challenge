0604ec2040372e133a7fe321516a9fecd040945b
YASSINE <contact@yassine.fun>
2024-05-12 06:23:39 -0700

    My square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
+"""Defines a square class."""
+
 
 class Square:
+    """Represents a square."""
 
     def __init__(self, side_length):
+        """Initializes a Square object."""
         self.side_length = side_length
 
     def area(self):
-        """ Area of the square """
+        """Calculates the area."""
         return self.side_length ** 2
 
     def perimeter(self):
+        """Calculates the perimeter."""
         return 4 * self.side_length
 
     def __str__(self):
+        """Returns string representation."""
         return f"Square with side length {self.side_length}"
 
 
 if __name__ == "__main__":
-
+    """Runs when directly executed."""
     s = Square(12)
     print(s)
     print(s.area())
