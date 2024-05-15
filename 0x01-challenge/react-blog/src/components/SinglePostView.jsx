bc68a30b11e6b22d266855b4e02f35933d632347
YASSINE <contact@yassine.fun>
2024-05-15 05:18:29 -0700

    Improve SinglePostView component and handle include rendering

--- 0x01-challenge/react-blog/src/components/SinglePostView.jsx
+++ 0x01-challenge/react-blog/src/components/SinglePostView.jsx
@@ -1,90 +1,83 @@
-var React = require('react/addons');
+var React = require('react');
 var SinglePostStore = require('../stores/SinglePostStore');
-var Glyphicon = require('react-bootstrap').Glyphicon;
 var SinglePostActions = require('../actions/SinglePostActions');
-var AllPostActions = require('../actions/AllPostActions');
-var Link = require('react-router').Link;
-var marked = require('marked');
 var AuthorMixin = require('../mixins/AuthorMixin.jsx');
 var JsxIncludes = require('./JsxIncludes');
+var marked = require('marked');
+var Link = require('react-router').Link;
 
 var SinglePostView = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object,
+  },
 
-    contextTypes: {
-        router: React.PropTypes.func
-    },
+  mixins: [AuthorMixin],
 
-    mixins: [AuthorMixin],
+  getInitialState: function () {
+    return SinglePostStore.getState();
+  },
 
-    componentWillMount: function() {
-        var self = this;
-        SinglePostActions.loadSinglePost(this.props.params.id,function(){
-            self.setState(SinglePostStore.getState());
-        });
-    },
+  componentDidMount: function () {
+    SinglePostStore.listen(this.onChange);
+    SinglePostActions.loadSinglePost(this.getPostId());
+  },
 
-    componentDidMount : function() {
-        SinglePostStore.listen(this.onChange);
-    },
+  componentWillUnmount: function () {
+    SinglePostStore.unlisten(this.onChange);
+    SinglePostActions.reset();
+  },
 
-    componentWillUnmount : function() {
-        SinglePostStore.unlisten(this.onChange);
-        SinglePostActions.reset();
-    },
+  onChange: function (state) {
+    this.setState(state);
+  },
 
-    onChange : function(state){
-        this.setState(state);
-    },
+  getPostId: function () {
+    return parseInt(this.props.params.id, 10);
+  },
 
-    getInitialState : function(){
-        return SinglePostStore.getState();
-    },
-
-    render : function() {
-        if(this.state.currentPost == null) {
-            return (<div></div>);
-        }
-
-        var includes = this.state.includes || [];
-
-        var htmlIncludes = [], mdIncludes = [], jsxIncludes = [];
-        if(includes.length > 0) {
-            var include;
-            for(var i=0; i<includes.length; i++) {
-                include = includes[i];
-                switch(include.type) {
-                    case 'html':
-                        htmlIncludes.push(include.value);
-                        break;
-                    case 'md':
-                        mdIncludes.push(include.value);
-                        break;
-                    case 'jsx':
-                        var Template = JsxIncludes[include.path];
-                        jsxIncludes.push(<Template key={include.path}/>);
-                        break;
-                }
-            }
-        }
-
-        return (
-            <div className="full-post">
-                <div>
-                    <Link to={`/`}><Glyphicon glyph="arrow-left" />&nbsp;Back</Link>
-                </div>
-                <h1 className="post-title">{this.state.currentPost.title}</h1>
-                <div className="author-details">
-                    {this.getAuthorDetails(this.state.currentPost)}
-                </div>
-                <div className="post-content">
-                    <div dangerouslySetInnerHTML={ {__html: this.state.currentPost.description || ''} }></div>
-                    <div dangerouslySetInnerHTML={ {__html: htmlIncludes.join('')} }></div>
-                    <div dangerouslySetInnerHTML={ {__html: marked( mdIncludes.join('') )} }></div>
-                    {jsxIncludes}
-                </div>
-            </div>
-        )
+  render: function () {
+    if (!this.state.currentPost) {
+      return <div></div>;
     }
+
+    var includes = this.state.includes || [];
+    var htmlIncludes = [];
+    var mdIncludes = [];
+    var jsxIncludes = [];
+
+    includes.forEach((include) => {
+      switch (include.type) {
+        case 'html':
+          htmlIncludes.push(include.value);
+          break;
+        case 'md':
+          mdIncludes.push(include.value);
+          break;
+        case 'jsx':
+          var Template = JsxIncludes[include.path];
+          jsxIncludes.push(<Template key={include.path} />);
+          break;
+      }
+    });
+
+    return (
+      <div className="full-post">
+        <div>
+          <Link to="/">
+            <span className="glyphicon glyphicon-arrow-left"></span> Back
+          </Link>
+        </div>
+        <h1 className="post-title">{this.state.currentPost.title}</h1>
+        <div className="author-details">{this.getAuthorDetails(this.state.currentPost)}</div>
+        <div className="post-content">
+          <div dangerouslySetInnerHTML={{ __html: this.state.currentPost.description || '' }} />
+          <div dangerouslySetInnerHTML={{ __html: htmlIncludes.join('') }} />
+          <div dangerouslySetInnerHTML={{ __html: marked(mdIncludes.join('')) }} />
+          {jsxIncludes}
+        </div>
+      </div>
+    );
+  },
 });
 
 module.exports = SinglePostView;
