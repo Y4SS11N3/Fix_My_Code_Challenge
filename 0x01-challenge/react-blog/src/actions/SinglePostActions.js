62355f2e5203eaf6b1476a0afe71fca16fb48758
YASSINE <contact@yassine.fun>
2024-05-15 05:17:26 -0700

    Refactor SinglePostActions for better code structure and error handling

--- 0x01-challenge/react-blog/src/actions/SinglePostActions.js
+++ 0x01-challenge/react-blog/src/actions/SinglePostActions.js
@@ -4,81 +4,79 @@ var config = require('../../config');
 var IncludeHandler = require('../IncludeHandler');
 
 class SinglePostActions {
+  loadSinglePost(id, cb) {
+    var SinglePostStore = require('../stores/SinglePostStore');
+    var state = SinglePostStore.getState();
 
-    loadSinglePost(id,cb){
-        var self = this;
+    if (state.stateById[id]) {
+      this.updateCurrentPost(state.stateById[id].post);
+      this.updateIncludes(state.stateById[id].includes);
+      if (cb) {
+        cb();
+      }
+    } else {
+      if (typeof window !== 'undefined' && typeof window.NProgress !== 'undefined') {
+        NProgress.start();
+      }
 
-        var SinglePostStore = require('../stores/SinglePostStore');
-        var state = SinglePostStore.getState();
-        if(!!state.stateById[id]) {
-            this.actions.updateCurrentPost(state.stateById[id].post);
-            this.actions.updateIncludes(state.stateById[id].includes);
-            if(cb){
-                cb();
-            }
-        } else {
-            if(typeof window != 'undefined' && typeof window.NProgress != 'undefined') {
-                NProgress.start();
-            }
-
-            request.get(config.baseUrl+'/ajax/post/'+id,function(err,response){
-                var post = response.body;
-                var includes = post.includes || [], loadedIncludes = [];
-                var includeNum  = includes.length;
-                
-                var finish = function() {
-                    self.actions.updateCurrentPost(post);
-                    self.actions.updateIncludes(loadedIncludes);
-                    setTimeout(function(){
-                        if(typeof NProgress != 'undefined') {
-                            NProgress.done();
-                        }
-                    },500);
-                    if(cb){
-                        cb();
-                    }
-                };
-
-                if(includeNum > 0) {
-
-                    var includeCallback = function(type, data, path) {
-                        loadedIncludes.push({
-                            type: type,
-                            value: data,
-                            path: path
-                        });
-
-                        includeNum --;
-                        if(includeNum == 0) {
-                            finish();
-                        }
-                    };
-
-                    var type, path;
-                    for(const i=0; i<includes.length; i++) {
-                        type = includes[i].type;
-                        path = includes[i].path;
-                        IncludeHandler.handleInclude(type, path, includeCallback);
-                    }
-                } else {
-                    finish();
-                }
-            });   
+      request.get(config.baseUrl + '/ajax/post/' + id, (err, response) => {
+        if (err) {
+          console.error(err);
+          return;
         }
-    }
 
-    updateCurrentPost(post){
-        this.dispatch(post);
+        var post = response.body;
+        var includes = post.includes || [];
+        var loadedIncludes = [];
+        var includeNum = includes.length;
+
+        var finish = () => {
+          this.updateCurrentPost(post);
+          this.updateIncludes(loadedIncludes);
+
+          setTimeout(() => {
+            if (typeof NProgress !== 'undefined') {
+              NProgress.done();
+            }
+          }, 500);
+
+          if (cb) {
+            cb();
+          }
+        };
+
+        if (includeNum > 0) {
+          includes.forEach((include) => {
+            var type = include.type;
+            var path = include.path;
+
+            IncludeHandler.handleInclude(type, path, (type, data, path) => {
+              loadedIncludes.push({ type: type, value: data, path: path });
+              includeNum--;
+
+              if (includeNum === 0) {
+                finish();
+              }
+            });
+          });
+        } else {
+          finish();
+        }
+      });
     }
-    
-    updateIncludes(includes) {
-        this.dispatch(includes);
-    }
-    
-    reset() {
-        this.dispatch();
-    }
+  }
+
+  updateCurrentPost(post) {
+    this.dispatch(post);
+  }
+
+  updateIncludes(includes) {
+    this.dispatch(includes);
+  }
+
+  reset() {
+    this.dispatch();
+  }
 }
-
 
 module.exports = alt.createActions(SinglePostActions);
