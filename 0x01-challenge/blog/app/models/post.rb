8ae8ee2f9d3de02ddf91647b827a1e63ae9c0ae3
YASSINE <contact@yassine.fun>
2024-05-14 22:49:45 -0700

    Add validation for online attribute in Post model

--- 0x01-challenge/blog/app/models/post.rb
+++ 0x01-challenge/blog/app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
-  has_many  :comments, dependent: :destroy
-  validates :title, presence: true, length: {minimum: 5}
-	validates :body,  presence: true
+  has_many :comments, dependent: :destroy
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :body, presence: true
+  validates :online, inclusion: { in: [true, false] }
 end
